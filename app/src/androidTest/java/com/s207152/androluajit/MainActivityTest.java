a64e86d524fa60d9ea3f69515cecfb91acee93be
furjoy <s207152@gmail.com>
2015-10-16 12:19:47 +0800

    Add the Android project

---
+++ app/src/androidTest/java/com/s207152/androluajit/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.s207152.androluajit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.s207152.androluajit.MainActivityTest \
+ * com.s207152.androluajit.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("com.s207152.androluajit", MainActivity.class);
+    }
+
+}
